457240bbf6cbf5b31b24641047eb9b33dfbf8eaa
tienthien <tienthienhd@gmail.com>
2018-11-10 09:08:28 +0700

    fontent book manage

--- resources/views/manager/books/edit.blade.php
+++ resources/views/manager/books/edit.blade.php
@@ -1,60 +1,103 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-</head>
-<body>
-	@if ($errors->any())
-	    <div class="alert alert-danger">
-	        <ul>
-	            @foreach ($errors->all() as $error)
-	                <li>{{ $error }}</li>
-	            @endforeach
-	        </ul>
-	    </div>
-	@endif
-	<form action="{{route('manager.book.update', ['book' => $book])}}" method="post" enctype="multipart/form-data">
-		@csrf
-		@method('put')
+@extends('manager.layouts.master')
 
-		{{__('validation.attributes.book.title')}}:
-		<input type="text" name="title" value="{{$book->title}}"><br>
-		{{__('validation.attributes.book.author')}}:
-		<input type="text" name="author" value="{{$book->author}}"><br>
-		{{__('validation.attributes.book.category')}}:
-		<select name='category'>
-			<option value="">{{__('messages.select-a-category')}}</option>
-			@foreach ( config('book-category') as $category)
-				<option value="{{$category}}"  {{ $book->category == $category ? 'selected' : '' }}>
-					{{__('book-category.'.$category)}}
-				</option>
-			@endforeach
-		</select><br>
-		{{__('validation.attributes.book.saleprice')}}:
-		<input type="number" name="saleprice" value="{{$book->saleprice}}"><br>
-		{{__('validation.attributes.book.purchasePrice')}}:
-		<input type="number" name="purchasePrice" value="{{$book->purchase_price}}"><br>
-		{{__('validation.attributes.book.description')}}:
-		<textarea rows="4" cols="50" name="description">{{$book->description}}</textarea><br>
-		{{__('validation.attributes.book.cover')}}:
-		<div id="imgCover" style="background-image: url('{{asset('/img/covers/'.$book->cover)}}');" class="img-thumbnail custom_cover" alt="Cover">
-            <div  class="custom_cover_imgae_space" ></div>
-            <div class=" row justify-content-center custom_link_upload_cover" >
-                <a style="color: white" href="#" onclick="$('#cover').click();">{{__('btn.upload-cover')}}</a></div>
-            <input id="cover" type="file" style="display: none" class="form-control{{ $errors->has('cover') ? ' is-invalid' : '' }}" name="cover" accept="image/*" >
+@section('content')
+<div class="page-content-wrapper">
+    <div class="page-content">
+        <div class="page-bar">
+            <div class="page-title-breadcrumb">
+                <div class=" pull-left">
+                    <div class="page-title">Edit Book</div>
+                </div>
+                <ol class="breadcrumb page-breadcrumb pull-right">
+                    <li><i class="fa fa-home"></i>&nbsp;<a class="parent-item" href="{{ route('manager.home') }}">Home</a>&nbsp;<i class="fa fa-angle-right"></i>
+                    </li>
+                    <li><a class="parent-item" href="{{ route('manager.book.index') }}">Book</a>&nbsp;<i class="fa fa-angle-right"></i>
+                    </li>
+                    <li class="active">Edit Book</li>
+                </ol>
+            </div>
+        </div>
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        <form action="{{route('manager.book.update', ['book' => $book])}}" method="post" enctype="multipart/form-data">
+        @csrf
+        @method('put')
 
-            @if ($errors->has('cover'))
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $errors->first('cover') }}</strong>
-                </span>
-            @endif
-            
-        </div>   
-		<input type="submit" name="editBook" value="{{__('btn.edit')}}">
-	</form>	
-	<script src="{{asset('js/jquery.min.js')}}"></script>
-	<script src="{{asset('js/preview_img.js')}}"></script>
-	<link rel="stylesheet" type="text/css" href="{{asset('css/mycss.css')}}">
-</body>
-</html>
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="card-box">
+                        <div class="card-head">
+                            <header>Book Detail: <q> {{ $book->title }} </q></header>
+                        </div>
+                        <div class="card-body row">
+                            <div class="col-md-6 col-sm-6 col-lg-6 p-t-20">
+                                <label>{{__('validation.attributes.book.title')}}</label>
+                                <input class="mdl-textfield__input" type="text" name="title" value="{{ $book->title }}" id="title">
+                            </div>
+                            <div class="col-md-6 col-sm-6 col-lg-6 p-t-20">
+                                <label>{{__('validation.attributes.book.author')}}</label>
+                                <input class="mdl-textfield__input" type="text" name="author" value="{{ $book->author }}" id="author">
+                            </div>
+                            <div class="col-md-6 col-sm-6 col-lg-6 p-t-20">
+                                <label>{{__('validation.attributes.book.category')}}</label>
+                                <select class="form-control select2 select2-hidden-accessible" tabindex="-1" aria-hidden="true" name="category">
+                                    <option value="">{{__('messages.select-a-category')}}</option>
+                                    @foreach ( config('book-category') as $category)
+                                        <option value="{{$category}}"  {{ $book->category == $category ? 'selected' : '' }}>
+                                            {{__('book-category.'.$category)}}
+                                        </option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="col-md-6 col-sm-6 col-lg-6 p-t-20">
+                                <label>{{__('validation.attributes.book.saleprice')}}</label>
+                                <input class="mdl-textfield__input" type="number" name="saleprice" value="{{ $book->saleprice }}" id="saleprice">
+                            </div>
+                            <div class="col-md-6 col-sm-6 col-lg-6 p-t-20">
+                                <label>{{__('validation.attributes.book.purchasePrice')}}</label>
+                                <input class="mdl-textfield__input" type="number" name="purchasePrice" value="{{ $book->purchase_price }}" id="purchase_price">
+                            </div>
+                            <div class="col-md-6 col-sm-6 col-lg-6 p-t-20">
+                                <label>State</label>
+                                <input class="mdl-textfield__input" type="text" name="state" value="{{($book->state < 0) ? '--' : $book->state}}" id="state" disabled>
+                            </div>
+                            <div class="col-md-6 col-sm-6 col-lg-6 p-t-20">
+                                <label>{{__('validation.attributes.book.cover')}}</label><br>
+                                <div id="imgCover" style="background-image: url('{{asset('/img/covers/'.$book->cover)}}'); width: 100%" class="img-thumbnail custom_cover" alt="Cover">
+                                    <div  class="custom_cover_imgae_space" ></div>
+                                    <div class=" row justify-content-center custom_link_upload_cover" >
+                                        <a style="color: white" href="#" onclick="$('#cover').click();">{{__('btn.upload-cover')}}</a></div>
+                                    <input id="cover" type="file" style="display: none" class="form-control{{ $errors->has('cover') ? ' is-invalid' : '' }}" name="cover" accept="image/*" >
+
+                                    @if ($errors->has('cover'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('cover') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-sm-6 col-lg-6 p-t-20">
+                                <label>{{__('validation.attributes.book.description')}}</label><br>
+                                <textarea class="mdl-textfield__input" rows="12" name="description">{{$book->description}}</textarea>
+                            </div>
+                            
+                            <div class="col-lg-12 p-t-20 text-center">
+                                <input class="mdl-button mdl-js-ripple-effect m-b-10 m-r-20 btn-pink" type="submit" name="editBook" value="{{__('btn.edit')}}">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> 
+        </form>
+    </div>
+</div>
+<script src="{{asset('js/preview_img.js')}}"></script>
+<link rel="stylesheet" type="text/css" href="{{asset('css/mycss.css')}}">
+@endsection
--- resources/views/manager/books/show.blade.php
+++ resources/views/manager/books/show.blade.php
@@ -1,36 +1,93 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-</head>
-<body>
-    @if (session('status'))
-        <div class="alert alert-success" role="alert">
-            {{ session('status') }}
+@extends('manager.layouts.master')
+
+@section('content')
+<div class="page-content-wrapper">
+    <div class="page-content">
+        <div class="page-bar">
+            <div class="page-title-breadcrumb">
+                <div class=" pull-left">
+                    <div class="page-title">Book Detail</div>
+                </div>
+                <ol class="breadcrumb page-breadcrumb pull-right">
+                    <li><i class="fa fa-home"></i>&nbsp;<a class="parent-item" href="{{ route('manager.home')}}">Home</a>&nbsp;<i class="fa fa-angle-right"></i>
+                    </li>
+                    <li><a class="parent-item" href="{{ route('manager.book.index')}}">Book</a>&nbsp;<i class="fa fa-angle-right"></i>
+                    </li>
+                    <li class="active">Book Detail</li>
+                </ol>
+            </div>
         </div>
-    @endif	
-	@if(isset($book))
-		{{$book->id}}
-		{{$book->title}}
-		{{$book->author}}
-		{{__('book-category.'.$book->category)}}
-		{{$book->cover}}
-		{{$book->description}}
-		{{__('word-and-statement.price', ['price' => number_format($book->saleprice, 0, '.', '.')])}}
-		{{__('word-and-statement.price', ['price' => number_format($book->purchase_price, 0, '.', '.')])}}
-		{{($book->state < 0) ? '--' : $book->state}}
-		<a href="{{route('manager.book.edit', ['book' => $book])}}">{{__('btn.edit')}}</a>
-		<form action="{{route('manager.book.update.off-state', ['book' => $book])}}" method="post">
-			@csrf
-			@method('put')
-			@if($book->state < 0)
-				<input type="submit" name="stopSaleBook" value="{{__('btn.stop-sale')}}" disabled="disabled">
-				<input type="submit" name="reRtopSaleBook" value="{{__('btn.sale-again')}}">
-			@else
-				<input type="submit" name="stopSaleBook" value="{{__('btn.stop-sale')}}">
-			@endif
-		</form>
-	@endif
-</body>
-</html>
+        @if (session('status'))
+            <div class="alert alert-success" role="alert">
+                {{ session('status') }}
+            </div>
+        @endif
+
+        @if(isset($book))
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="card-box">
+                    <div class="card-head">
+                        <header>Book Detail: <q> {{ $book->title }} </q></header>
+                    </div>
+
+                    <div class="card-body row">
+                        <div class="col-md-6 col-sm-6 col-lg-6 p-t-20">
+                            <label>ID</label>
+                            <input class="mdl-textfield__input" type="text" name="id" value="{{ $book->id }}" id="id" disabled>
+                        </div>
+                        <div class="col-md-6 col-sm-6 col-lg-6 p-t-20">
+                            <label>Title</label>
+                            <input class="mdl-textfield__input" type="text" name="title" value="{{ $book->title }}" id="title" disabled>
+                        </div>
+                        <div class="col-md-6 col-sm-6 col-lg-6 p-t-20">
+                            <label>Author</label>
+                            <input class="mdl-textfield__input" type="text" name="author" value="{{ $book->author }}" id="author" disabled>
+                        </div>
+                        <div class="col-md-6 col-sm-6 col-lg-6 p-t-20">
+                            <label>Category</label>
+                            <input class="mdl-textfield__input" type="text" name="category" value="{{__('book-category.'.$book->category)}}" id="category" disabled>
+                        </div>
+                        <div class="col-md-6 col-sm-6 col-lg-6 p-t-20">
+                            <label>Sale price</label>
+                            <input class="mdl-textfield__input" type="text" name="saleprice" value="{{__('word-and-statement.price', ['price' => number_format($book->saleprice, 0, '.', '.')])}}" id="saleprice" disabled>
+                        </div>
+                        <div class="col-md-6 col-sm-6 col-lg-6 p-t-20">
+                            <label>Purchase price</label>
+                            <input class="mdl-textfield__input" type="text" name="purchase_price" value="{{__('word-and-statement.price', ['price' => number_format($book->purchase_price, 0, '.', '.')])}}" id="purchase_price" disabled>
+                        </div>
+                        <div class="col-md-6 col-sm-6 col-lg-6 p-t-20">
+                            <label>State</label>
+                            <input class="mdl-textfield__input" type="text" name="state" value="{{($book->state < 0) ? '--' : $book->state}}" id="state" disabled>
+                        </div>
+                        <div class="col-md-6 col-sm-6 col-lg-6 p-t-20"></div>
+
+                        <div class="col-md-6 col-sm-6 col-lg-6 p-t-20">
+                            <label>Cover</label><br>
+                            <img src="{{ asset("img/covers/" . $book->cover) }}" width="80%">
+                        </div>
+                        <div class="col-md-6 col-sm-6 col-lg-6 p-t-20">
+                            <label>Description</label><br>
+                            <textarea class="mdl-textfield__input" rows="12" name="description" disabled>{{$book->description}}</textarea>
+                        </div>
+                        <div class="col-lg-12 p-t-20 text-center"> 
+                            <a class="mdl-button mdl-js-ripple-effect m-b-10 m-r-20 btn-pink" href="{{route('manager.book.edit', ['book' => $book])}}" style="color: #ffffff">{{__('btn.edit')}}</a>
+                            <form action="{{route('manager.book.update.off-state', ['book' => $book])}}" method="post">
+                                @csrf
+                                @method('put')
+                                @if($book->state < 0)
+                                    <input class="mdl-button mdl-js-ripple-effect m-b-10 m-r-20 btn-pink" type="submit" name="stopSaleBook" value="{{__('btn.stop-sale')}}" disabled="disabled">
+                                    <input class="mdl-button mdl-js-ripple-effect m-b-10 m-r-20 btn-pink" type="submit" name="reRtopSaleBook" value="{{__('btn.sale-again')}}">
+                                @else
+                                    <input class="mdl-button mdl-js-ripple-effect m-b-10 m-r-20 btn-pink" type="submit" name="stopSaleBook" value="{{__('btn.stop-sale')}}">
+                                @endif
+                            </form>
+                           </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @endif
+    </div>
+</div>
+@endsection
