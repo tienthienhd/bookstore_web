e29d59b3dc597dba8b5e0e881045985b5172c113
haind146 <nguyenduchai97@gmail.com>
2018-11-15 22:24:49 +0700

    add home and book detail screen

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -1,39 +1,129 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-</head>
-<body>
+@extends('layouts.app')
+<link href="{{ asset('css/book.css') }}" rel="stylesheet">
+@section('content')
     @if (session('status'))
         <div class="alert alert-success" role="alert">
             {{ session('status') }}
         </div>
     @endif
     @if ($errors->any())
-	    <div class="alert alert-danger">
-	        <ul>
-	            @foreach ($errors->all() as $error)
-	                <li>{{ $error }}</li>
-	            @endforeach
-	        </ul>
-	    </div>
-	@endif	
-	@if(isset($book))
-		<a href="{{route('book.comment',['bookId' => $book->id])}}">{{__('btn.comment-detail')}}</a><br>
-		{{$book->title}}
-		{{$book->author}}
-		{{__('book-category.'.$book->category)}}
-		{{$book->cover}}
-		{{$book->description}}
-		{{__('word-and-statement.price', ['price' => number_format($book->saleprice, 0, '.', '.')])}}
-		{{$star}}
-		<form action="{{route('cart.add')}}" method="post">
-			@csrf
-			<input type="hidden" name="bookId" value="{{$book->id}}">
-			<input type="hidden" name="quantity" value="1">
-			<input type="submit" name="addToCart" value="{{__('btn.add-to-card')}}">	
-		</form>
-	@endif
-</body>
-</html>
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    @if(isset($book))
+        <div class="container">
+            <div class="row book-info">
+                <div class="col-md-4">
+                    <img src="{{asset("img/covers/" . $book->cover)}}">
+                </div>
+                <div class="col-md-8">
+                    <h4>{{$book->title}}</h4>
+                    <div class="star-rated">
+                        <i class="material-icons checked">grade</i>
+                        <i class="material-icons checked">grade</i>
+                        <i class="material-icons checked">grade</i>
+                        <i class="material-icons">grade</i>
+                        <i class="material-icons">grade</i>
+                        <span>(50 đánh giá)</span>
+                    </div>
+                    <div class="price">
+                        {{__('word-and-statement.price', ['price' => number_format($book->saleprice, 0, '.', '.')])}}
+                    </div>
+                    <table>
+                        <tr>
+                            <th>Tác giả:</th>
+                            <td>{{$book->author}}</td>
+                        </tr>
+                        <tr>
+                            <th>Nhà xuất bản:</th>
+                            <td>dsfsdf</td>
+                        </tr>
+                        <tr>
+                            <th>Kích thước:</th>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <th>Loại bìa:</th>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <th>Số trang:</th>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <th>SKU:</th>
+                            <td></td>
+                        </tr>
+                    </table>
+                    <form action="{{route('cart.add')}}" method="post">
+                        @csrf
+                        <input type="hidden" name="bookId" value="{{$book->id}}">
+                        <input type="hidden" name="quantity" value="1">
+                        <button type="submit" class="add-to-cart"><i class="material-icons">shopping_cart</i><span>Thêm vào giỏ</span>
+                        </button>
+                    </form>
+                </div>
+            </div>
+            <br>
+            <h5>GIỚI THIỆU SÁCH</h5>
+            <div class="row book-description">
+                {{$book->description}}
+            </div>
+            <br>
+            <h5>ĐÁNH GIÁ</h5>
+            <div class="row review">
+                <div class="row review-item">
+                    <div class="col-md-2">
+                        <img src="https://cdn3.iconfinder.com/data/icons/emoticons-23/64/_Ninja-512.png">
+                    </div>
+                    <div class="col-md-7">
+                        <h6>Sasuke</h6>
+                        <p>Japan</p>
+                    </div>
+                    <div class="col-md-3">
+                        <i class="material-icons checked">grade</i>
+                        <i class="material-icons checked">grade</i>
+                        <i class="material-icons checked">grade</i>
+                        <i class="material-icons">grade</i>
+                        <i class="material-icons">grade</i>
+                    </div>
+                    <p style="margin: 1em 3em 1em 3em">blaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasssssssaaaaaaaaaaaaaaaa</p>
+                </div>
+            </div>
+            <h5 style="margin: 1em 0 1em 0">VIẾT NHẬN XÉT</h5>
+            <div class="row write-review">
+                <form>
+                    <div class="form-group row">
+                        <label  class="col-sm-2 col-form-label">Đánh giá của bạn</label>
+                        <div class="col-sm-10">
+                            <fieldset class="rating">
+                                <input type="radio" id="star5" name="rating" value="5" /><label for="star5" title="Rocks!">5 stars</label>
+                                <input type="radio" id="star4" name="rating" value="4" /><label for="star4" title="Pretty good">4 stars</label>
+                                <input type="radio" id="star3" name="rating" value="3" /><label for="star3" title="Meh">3 stars</label>
+                                <input type="radio" id="star2" name="rating" value="2" /><label for="star2" title="Kinda bad">2 stars</label>
+                                <input type="radio" id="star1" name="rating" value="1" /><label for="star1" title="Sucks big time">1 star</label>
+                            </fieldset>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="inputPassword3" class="col-sm-2 col-form-label">Nhận xét</label>
+                        <div class="col-sm-10">
+                            <textarea type="text" class="form-control"> </textarea>
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <div class="col-sm-10 offset-2">
+                            <button type="submit" class="btn btn-outline-info">Nhận xét</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    @endif
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,63 +18,110 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/index.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+<div id="app">
+    <nav class="navbar navbar-expand-md navbar-laravel navbar-customer">
+        <div class="container">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                {{ config('app.name', 'Laravel') }}
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
+                    <div class="dropdown">
+                        <button type="button" class="book-genre dropdown-toggle" data-toggle="dropdown">
+                            THỂ LOẠI
+                        </button>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">Link 1</a>
+                            <a class="dropdown-item" href="#">Link 2</a>
+                            <a class="dropdown-item" href="#">Link 3</a>
+                        </div>
+                    </div>
+                </ul>
 
-                    </ul>
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item" ><input style="width: 18rem" class="search" type="text" placeholder="Tìm kiếm sách/tác giả"></li>
+                    <li class="nav-item"><button class="material-icons search">search</button></li>
+                </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            <li class="nav-item">
-                                @if (Route::has('register'))
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                @endif
-                            </li>
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->username }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a>
+                            <i class="material-icons">track_changes</i>
+                            <span>Theo dõi đơn hàng</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{route('cart.index')}}">
+                            <i class="material-icons">shopping_cart</i>
+                            <div class="number-noti">1</div>
+                            <span style="left: -10px">Giỏ hàng</span>
+                        </a>
+                    </li>
+                </ul>
             </div>
-        </nav>
+        </div>
+    </nav>
+    <nav class="mini-nav">
+        <div class="container">
+            <ul>
+                <!-- Authentication Links -->
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Đăng nhập') }}</a>
+                    </li>
+                    <li class="nav-item">
+                        @if (Route::has('register'))
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Đăng ký') }}</a>
+                        @endif
+                    </li>
+                @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->username }} <span class="caret"></span>
+                        </a>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+
+                            <a class="dropdown-item" href="{{route('customer.waitcommentlist')}}"
+                               title=""> {{__('word-and-statement.wait-comment')}} </a>
+                            <a class="dropdown-item" href="{{route('customer.comment.index')}}"
+                               title=""> {{__('word-and-statement.list-comment')}} </a>
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                {{ __('Đăng xuất') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @endguest
+            </ul>
+        </div>
+    </nav>
+
+    <main class="main-content">
+        @yield('content')
+    </main>
+    <footer>
+
+    </footer>
+</div>
 </body>
 </html>
