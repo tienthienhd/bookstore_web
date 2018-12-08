2b186fad362248bcb5c17cbe18bd8f47c8c36ca3
tienthien <tienthienhd@gmail.com>
2018-12-08 13:44:41 +0700

    fix database config

--- config/database.php
+++ config/database.php
@@ -41,7 +41,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'ec2-54-197-249-140.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
@@ -53,7 +53,10 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
+            'options'   => [PDO::ATTR_EMULATE_PREPARES => true],
         ],
+
+
 
         'pgsql' => [
             'driver' => 'pgsql',
